181a4c2c99a0d444a530bf330e50f2f60a3eb024
chris.kearse <chris.kearse@gmail.com>
2018-05-16 14:36:23 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const socket = require('socket.io');
+
+//app.use(parser({extended: true}));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/static'));
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.post('/chat', (req, res) => {
+  //username = req.body.username;
+  
+  res.render('chat', {'username': req.body.username});
+});
+
+const server = app.listen(7777, function() {
+  console.log('Express app running on port 7777');
+});
+
+var io = socket(server);
+
+io.on('connection', socket => {
+  console.log('socket connection established!');
+
+  socket.on('chat', data => {
+    io.emit('chat', {
+      username: data.username,
+      message: data.message
+    });
+
+    console.log("message: " + data.message);
+
+  })
+
+})
+
+
