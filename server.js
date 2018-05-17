e247254b4786c91c7c86ac65b923c3afafc8d79c
chris.kearse <chris.kearse@gmail.com>
2018-05-16 21:27:20 -0700

    Added unique chat bubble colors to more easily visually group them based on the sender, added scrollbar styling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const socket = require('socket.io');
+const randomColor = require('randomcolor');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -10,7 +11,15 @@ app.use(express.static(__dirname + '/static'));
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.post('/chat', (req, res) => {
-  res.render('chat', {'username': req.body.username});
+  var chat_color  = randomColor();
+
+  //add slight opacity via alpha channel ~20%
+  chat_color += "33";
+
+  res.render('chat', {
+    'username': req.body.username,
+    'unq_color': chat_color  
+  });
 });
 
 const server = app.listen(7777, function() {
@@ -27,13 +36,13 @@ io.on('connection', socket => {
   }),
 
   socket.on('chat', data => {
-    io.emit('chat', {
-      username: data.username,
-      message: data.message
-    });
+    // io.emit('chat', {
+    //   username: data.username,
+    //   message: data.message,
+    //   unique_chat_color: data.unique_chat_color
+    // });
 
-    console.log("message: " + data.message);
-
+    io.emit('chat', data);
   })
 
 })
